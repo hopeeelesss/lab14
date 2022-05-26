2b29db9ace63e20fcc28fa96bcc8b22eeb5a967e
Victor <khokhlov34@gmail.com>
2022-05-27 00:24:53 +0300

    19 прак

--- lab14/src/main/java/com/example/lab14/services/DoctorService.java
+++ lab14/src/main/java/com/example/lab14/services/DoctorService.java
@@ -2,6 +2,8 @@ package com.example.lab14.services;
 
 import com.example.lab14.entities.Doctor;
 import com.example.lab14.repos.DoctorRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -9,22 +11,24 @@ import javax.transaction.Transactional;
 
 @Service
 @Transactional
+@Slf4j
 public class DoctorService {
-    private final DoctorRepo doctorRepo;
+    @Autowired
+    private DoctorRepo doctorRepo;
 
-    public DoctorService(DoctorRepo doctorRepo) {
-        this.doctorRepo = doctorRepo;
-    }
 
     public void add(Doctor doctor){
+        log.info("Added {}", doctor);
         doctorRepo.save(doctor);
     }
 
     public void delete(Doctor doctor){
+        log.info("Deleted {}",doctor);
         doctorRepo.delete(doctor);
     }
 
     public void list(){
+        log.info("Listed Doctors");
         for(Doctor doctor: doctorRepo.findAll()){
             System.out.println(doctor);
         }
--- lab14/src/main/java/com/example/lab14/services/PatientService.java
+++ lab14/src/main/java/com/example/lab14/services/PatientService.java
@@ -2,28 +2,32 @@ package com.example.lab14.services;
 
 import com.example.lab14.entities.Patient;
 import com.example.lab14.repos.PatientRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
 @Transactional
+@Slf4j
 public class PatientService {
-    private final PatientRepo patientRepo;
+    @Autowired
+    private PatientRepo patientRepo;
 
-    public PatientService(PatientRepo patientRepo) {
-        this.patientRepo = patientRepo;
-    }
 
     public void add(Patient patient){
+        log.info("Added {}", patient);
         patientRepo.save(patient);
     }
 
     public void delete(Patient patient){
+        log.info("Deleted {}", patient);
         patientRepo.delete(patient);
     }
 
     public void list(){
+        log.info("Listed Patients");
         for(Patient patient: patientRepo.findAll()){
             System.out.println(patient);
         }
